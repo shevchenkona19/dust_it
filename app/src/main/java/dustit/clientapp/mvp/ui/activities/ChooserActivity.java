eb4c45c21735982fe6cfde6f8ccfbf86d8926783
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-09-28 21:07:27 +0300

    Design rework at: Chooser, Login, Register

--- app/src/main/java/dustit/clientapp/mvp/ui/activities/ChooserActivity.java
+++ app/src/main/java/dustit/clientapp/mvp/ui/activities/ChooserActivity.java
@@ -1,10 +1,11 @@
 package dustit.clientapp.mvp.ui.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,18 +16,37 @@ public class ChooserActivity extends AppCompatActivity {
     Button btnLogin;
     @BindView(R.id.btnChooserRegister)
     Button btnRegister;
+    @BindView(R.id.ivChooserIcon)
+    ImageView ivIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooser);
         ButterKnife.bind(this);
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Explode fade = new Explode();
+            fade.excludeTarget(R.id.clChooserLayout, true);
+            fade.excludeTarget(android.R.id.statusBarBackground, true);
+            fade.excludeTarget(android.R.id.navigationBarBackground, true);
+            fade.excludeTarget(android.R.id.background, true);
+            getWindow().setExitTransition(fade);
+            getWindow().setEnterTransition(fade);
+        }*/
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Pair pair1 = Pair.create(btnLogin, "btnLogin");
+                    Pair pair2 = Pair.create(ivIcon, "icon");
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(ChooserActivity.this, pair1, pair2);
+                    Intent intent = new Intent(ChooserActivity.this, LoginActivity.class);
+                    startActivity(intent, options.toBundle());
+                    finish();
+                    return;
+                }*/
                 Intent intent = new Intent(ChooserActivity.this, LoginActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
 
@@ -35,7 +55,6 @@ public class ChooserActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(ChooserActivity.this, RegisterActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
     }
--- app/src/main/java/dustit/clientapp/mvp/ui/activities/LoginActivity.java
+++ app/src/main/java/dustit/clientapp/mvp/ui/activities/LoginActivity.java
@@ -1,11 +1,15 @@
 package dustit.clientapp.mvp.ui.activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Fade;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -28,6 +32,15 @@ public class LoginActivity extends AppCompatActivity implements ILoginActivityVi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Explode fade = new Explode();
+            fade.excludeTarget(R.id.clLoginLayout, true);
+            fade.excludeTarget(android.R.id.statusBarBackground, true);
+            fade.excludeTarget(android.R.id.navigationBarBackground, true);
+            fade.excludeTarget(android.R.id.background, true);
+            getWindow().setEnterTransition(fade);
+            getWindow().setExitTransition(fade);
+        }*/
         loginActivityPresenter.bind(this);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +61,11 @@ public class LoginActivity extends AppCompatActivity implements ILoginActivityVi
     public void onLoggedSuccessfully() {
         Intent intent = new Intent(this, FeedActivity.class);
         startActivity(intent);
-        finish();
     }
 
     @Override
     public void onError(String message) {
         Toast.makeText(this, "ERROR\n" + message, Toast.LENGTH_SHORT).show();
     }
+
 }
