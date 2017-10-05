702d2b2d1ef0c86ed405f33846df534711bb6887
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-10-05 10:56:59 +0300

    Feed basic implementation

---
+++ app/src/main/java/dustit/clientapp/mvp/ui/adapters/FeedRecyclerViewAdapter.java
@@ -0,0 +1,9 @@
+package dustit.clientapp.mvp.ui.adapters;
+
+/**
+ * Created by shevc on 05.10.2017.
+ * Let's GO!
+ */
+
+public class FeedRecyclerViewAdapter {
+}
--- app/src/main/java/dustit/clientapp/mvp/ui/fragments/FeedFragment.java
+++ app/src/main/java/dustit/clientapp/mvp/ui/fragments/FeedFragment.java
@@ -4,14 +4,24 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import dustit.clientapp.R;
+import dustit.clientapp.mvp.ui.interfaces.IFeedFragmentView;
 
 
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements IFeedFragmentView{
+
+    @BindView(R.id.rvFeed)
+    RecyclerView rvFeed;
+
+    private Unbinder unbinder;
 
 
     private IFeedFragmentInteractionListener interactionListener;
@@ -28,8 +38,9 @@ public class FeedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View v = inflater.inflate(R.layout.fragment_feed, container, false);
+        unbinder = ButterKnife.bind(this, v);
+        return v;
     }
 
 
